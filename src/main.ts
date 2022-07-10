ed99914571b06a36bfd3ffd7b022725cdf786ae1
Aleksandr Svistunov <aleksandr.svistunov@quadcode.com>
2022-07-10 23:19:35 +0300

    implemented httpException filter

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transformResponse/transformResponse.interceptor';
 import { CatchErrorInterceptor } from './common/interceptors/catchError/catchError.interceptor';
+import { HttpExceptionFilter } from './common/filters/httpException/httpException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalInterceptors(
     new TransformInterceptor(),
     new CatchErrorInterceptor(),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
