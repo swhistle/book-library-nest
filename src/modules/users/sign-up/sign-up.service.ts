335e833ffb3bccdde45e9202d0b99b07592d5d18
Aleksandr Svistunov <aleksandr.svistunov@quadcode.com>
2022-07-17 20:13:47 +0300

    added hashing password and salt for new user

--- src/modules/users/sign-up/sign-up.service.ts
+++ src/modules/users/sign-up/sign-up.service.ts
@@ -1,16 +1,30 @@
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { Injectable } from '@nestjs/common';
-import { IUser } from '../interfaces/user.interface';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { IUser, IUserPublicData } from '../interfaces/user.interface';
 import { User, UserDocument } from '../schemas/user.schema';
 
 @Injectable()
 export class SignUpService {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-    async signUp(user: IUser): Promise<User> {
-        console.log('signUpService signUp user', user);
-        const newUser = new this.userModel(user);
-        return newUser.save();
+    async signUp(user: IUser): Promise<IUserPublicData> {
+        const {email, password, firstName, lastName} = user;
+
+        try {
+            const newUser = new this.userModel({email, firstName, lastName});
+            newUser.setPassword(password);
+
+            await newUser.save();
+
+            return {
+                email: newUser.email,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName
+            };
+        } catch (e) {
+            throw new BadRequestException(e);
+        }
+
     }
 }
